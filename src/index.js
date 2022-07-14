ae1e83ff18cea57c854c1931e1106134d3456386
Roger Gee <rpg11a@acu.edu>
2022-07-14 17:32:43 -0500

    Initial commit: Add package.json and entry module

---
+++ src/index.js
@@ -0,0 +1,22 @@
+/**
+ * main.js
+ *
+ * rollup-plugin-package-bundle
+ */
+
+export function packageBundle(options) {
+
+  return {
+    name: "tccl-package-bundle",
+
+    async resolveId(source,importer,options) {
+
+    },
+
+    load(id) {
+
+    }
+  };
+}
+
+export default packageBundle;
